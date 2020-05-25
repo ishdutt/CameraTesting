8c9fffc6ee3937765d5d3cf185f1d7656a3413d4
ishdutt <trivediishdutt@gmail.com>
2020-05-25 08:40:06 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    //Downloading the image
+    // var image = cameraSensor.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    // window.location.href=image; // it will save locally
+    cameraOutput.setAttribute('download', 'MintyPaper.png');
+    // cameraOutput.setAttribute('src', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+    // cameraOutput.click();
+    var image = cameraSensor.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    window.location.href=image; // it will save locally
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
